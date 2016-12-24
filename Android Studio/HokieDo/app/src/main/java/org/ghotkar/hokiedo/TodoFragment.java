d7d6e58472fca2cca774fe6b5a5c4afc228389ab
Chinmay <chinmayg@vt.edu>
2016-12-24 18:31:30 -0500

    New Navigation Method
    Committing working login fragment and pref fragment

---
+++ Android Studio/HokieDo/app/src/main/java/org/ghotkar/hokiedo/TodoFragment.java
@@ -0,0 +1,36 @@
+package org.ghotkar.hokiedo;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ghotkar.testnavigationdrawer.R;
+
+/**
+ * Created by cghotkar on 12/24/16.
+ */
+
+public class TodoFragment extends Fragment {
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.todo_fragment, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        return v;
+    }
+}
